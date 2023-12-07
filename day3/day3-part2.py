844fd1f84d44e336a6aa8580c19c95a71ec6995b
jeremyout <22989572+jeremyout@users.noreply.github.com>
2023-12-06 23:49:22 -0600

    Day 3 part 2 solution
    :grimacing:

--- day3/day3-part2.py
+++ day3/day3-part2.py
@@ -1,10 +1,13 @@
 input_file = open("input.txt", "r")
 # input_file = open("example_input.txt", "r")
 
-symbol_list = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "*", "-", "+", "=", "/"]
-symbol_index_to_process = []
-valid_engine_numbers = []
+symbol_list = ["*"]
+gear_adjacent_numbers = []
+gear_locations = []
 line_data = input_file.readlines()
+
+def logGearLocation(line, index):
+    gear_locations.append([line,index])
     
 def checkForAdjacentOrDiagonalSymbol(current_line_number, start_index, end_index):
     adjacent_and_diagnonal_chars = []
@@ -15,22 +18,32 @@ def checkForAdjacentOrDiagonalSymbol(current_line_number, start_index, end_index
     if start_index != 0:
         left_adjacent = current_line_data[start_index-1]
         adjacent_and_diagnonal_chars.append(left_adjacent)
+        if left_adjacent in symbol_list:
+            logGearLocation(current_line_number, start_index-1)
     
     if end_index != (len(current_line_data)-1):
         right_adjacent = current_line_data[end_index+1]
         adjacent_and_diagnonal_chars.append(right_adjacent)
+        if right_adjacent in symbol_list:
+            logGearLocation(current_line_number, end_index+1)
+ 
     
     if current_line_number != 0:
         previous_line = line_data[current_line_number-1].strip()
         for index, char in enumerate(previous_line):
             if (index >= start_index-1) and (index <= end_index+1):
                 adjacent_and_diagnonal_chars.append(char)
+                if char in symbol_list:
+                    logGearLocation(current_line_number-1, index)
+                
 
     if current_line_number != (len(line_data)-1):
         next_line = line_data[current_line_number+1].strip()
         for index, char in enumerate(next_line):
             if (index >= start_index-1) and (index <= end_index+1):
                 adjacent_and_diagnonal_chars.append(char)
+                if char in symbol_list:
+                    logGearLocation(current_line_number+1, index)
 
     for entry in adjacent_and_diagnonal_chars:
         if entry in symbol_list:
@@ -58,15 +71,31 @@ for line_number,line_content in enumerate(line_data):
             start_index_logged = False
             number_end_index = index-1
             if checkForAdjacentOrDiagonalSymbol(line_number, number_start_index, number_end_index):
-                valid_engine_numbers.append(''.join(current_number))
+                gear_adjacent_numbers.append(''.join(current_number))
             current_number = []
 
-sum = 0
-for num in valid_engine_numbers:
-    sum += int(num)
+counter = 0
+matched_entries = []
+for i in range(len(gear_locations)):
+    for j in range(i+1, (len(gear_locations))):
+        if gear_locations[i] == gear_locations[j]:
+            counter += 1
+            saved = gear_adjacent_numbers[j]         
+    if counter == 1:
+        counter = 0
+        matched_entries.append([gear_adjacent_numbers[i], saved])
+
+products = []
+sum_of_products = 0
+for entry in matched_entries:
+    sum_of_products += int(entry[0]) * int(entry[1])
+
+print(f"Gear locations: {gear_locations}")
+print(f"Gear adjacent numbers: {gear_adjacent_numbers}")
+
 
 print("==========================================")
 print("==========================================")
-print(f"Sum of valid engine numbers: {sum}")
+print(f"Sum of gear ratios: {sum_of_products}")
 print("==========================================")
 print("==========================================")
